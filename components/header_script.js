443ba589a0f5fc7bae01f9864e6686342015c54a
MartinAavastik <martin@avalah.ee>
2025-11-30 19:34:13 +0200

    uuenda path-e alam html-idel õigesti

--- components/header_script.js
+++ components/header_script.js
@@ -1,6 +1,6 @@
 
 // Load and insert header
-fetch('/components/header.html')
+fetch('../../components/header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header').innerHTML = data;
